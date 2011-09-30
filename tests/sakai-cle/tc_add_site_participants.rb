0f69996828f007e1086f89ad45bf4908211e0ba6
Abraham Heward <aheward@rsmart.com>
2011-09-30 15:46:59 -0700

    Completed the tc_add_site_participants script, in its basic form
    It's still going to need some test verifications and another test for
    selecting roles individually, but for now it works to add the people
    listed in the directory.yml file without error.

--- tests/sakai-cle/tc_add_site_participants.rb
+++ tests/sakai-cle/tc_add_site_participants.rb
@@ -38,35 +38,114 @@ class AddSiteParticipants < Test::Unit::TestCase
   
   def test_adding_participants
     
+    # Prepare the test case data...
+    # Get participants and stick them in an array
+    students = []
+    instructors = []
+    guests = []
+    
+    x = 1
+    
+    while @config.directory["person#{x}"] != nil do
+      
+      type = @config.directory["person#{x}"]["type"]
+      id = @config.directory["person#{x}"]["id"]
+      case(type)
+      when "registered" then students << id
+      when "guest" then guests << id
+      when "maintain" then instructors << id
+      end
+      
+      x+=1
+      
+    end
+    
+    # Now put the names into a string that can be
+    # entered into the appropriate text
+    # field later
+    students_list = students.join("\n")
+    instructors_list = instructors.join("\n")
+    guests_list = guests.join("\n")
+    
+    # Now, the strings go into a hash for iteration through the tests.
+    users = { :students=>students_list, :instructors=>instructors_list, :guests=>guests_list }
+    
     # Log in to Sakai
     @sakai.login(@user_name, @password)
     
     # Go to Site Setup
     home = Home.new(@browser)
     home.site_setup
-    
+   
     # Define the frame for ease of code writing (and reading)
     def frm
       @browser.frame(:index=>0)
     end
-    
+
     # Narrow down and sort the list to easily
     # find the test site
-    site_setup = SiteSetup.new(@browser)
-    site_setup.view="course Sites"
-    2.times{site_setup.sort_by_creation_date}
+    frm.select(:id, "view").select("course Sites")
     
+    site_setup = SiteSetup.new(@browser)
+
+    2.times{site_setup.sort_by_creation_date}
+      
     # Get the site id so that we can check the right checkbox
     frm.link(:text, @site_name).href =~ /(?<=\/site\/).+/
     site_id = $~.to_s
-    
+      
     # Check the checkbox
     frm.checkbox(:value, site_id).set
-    
+      
     # Edit the site
     site_setup.edit
-    
-    
+
+    users.each do | user_type, user_list |
+      
+      next if user_list==""
+      
+      edit_site = SiteSetupEdit.new(@browser)
+      
+      # Add the participants
+      edit_site.add_participants
+      add_participants = SiteSetupAddParticipants.new(@browser)
+      
+      # Enter the names into the official participants field
+      add_participants.official_participants=user_list
+      add_participants.continue
+      
+      # Choose the role
+      role = SiteSetupChooseRole.new(@browser)
+      
+      case(user_type)
+      when :guests then role.select_guest
+      when :instructors then role.select_instructor
+      when :students then role.select_student
+      else
+        p user_type
+        role.select_teaching_assistant
+      end
+      
+      role.continue
+      
+      # Don't send an email
+      email = SiteSetupParticipantEmail.new(@browser)
+      email.continue
+      
+      # Confirm selections
+      confirm = SiteSetupParticipantConfirmation.new(@browser)
+      
+      #===================
+      # Need to add verification steps here!
+      #===================
+      
+      confirm.finish
+      
+      #===================
+      # Need to add verification steps here!
+      #===================
+      
+    end
     
   end
   
