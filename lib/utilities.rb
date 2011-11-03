abb7cc4b0d7a20d75b8dbb4285ea6459cc412bfc
Abraham Heward <aheward@rsmart.com>
2011-11-02 17:21:10 -0700

    About 2/3rds done making class instance variables for the test cases.
    Added a new method to utilities called get_filename that will allow
    extracting the file name out of the path + name for file uploads.

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -6,6 +6,17 @@
 
 module Utilities
   
+  # Strips the file name away from the path information.
+  #
+  # This way it's not necessary to define variables for BOTH the
+  # file name and the file path + file name. Just define the
+  # path + name and then use this method to extract only the filename
+  # portion.
+  def get_filename(path_plus_name_string)
+    path_plus_name_string =~ /(?<=\/).+/
+    return $~.to_s
+  end
+  
   # A random string creator that draws from all printable ASCII characters
   # from 33 to 128.
   def random_string(length=10, s="")
