a0e81079c89d76a9b34e94ddf0006099cb7dbc52
Abraham Heward <aheward@rsmart.com>
2011-11-18 16:17:48 -0700

    I'm going to need help understanding how to keep certain files out of the commit list, because I can't seem to keep git from wanting me to commit this file.

--- tests/sakai-cle/test.rb
+++ tests/sakai-cle/test.rb
@@ -1,70 +1,53 @@
-# 
-# == Synopsis
-#
-# This script uploads resources to a test site and
-# sets them all to publicly viewable status.
-# 
-# Author: Abe Heward (aheward@rSmart.com)
-gem "test-unit"
-gems = ["test/unit", "watir-webdriver"]
-gems.each { |gem| require gem }
-files = [ "/../../config/config.rb", "/../../lib/utilities.rb", "/../../lib/sakai-CLE/app_functions.rb", "/../../lib/sakai-CLE/admin_page_elements.rb", "/../../lib/sakai-CLE/site_page_elements.rb", "/../../lib/sakai-CLE/common_page_elements.rb" ]
-files.each { |file| require File.dirname(__FILE__) + file }
-require "ci/reporter/rake/test_unit_loader"
-
-class AddPublicResources < Test::Unit::TestCase
-  
-  include Utilities
-
-  def setup
-    
-    # Get the test configuration data
-    @config = AutoConfig.new
-    @browser = @config.browser
-    # This test case uses an admin login
-    @username = @config.directory['admin']['username']
-    @password = @config.directory['admin']['password']
-    @site_name = @config.directory['site1']['name']
-    @site_id = @config.directory['site1']['id']
-    @sakai = SakaiCLE.new(@browser)
-    
-    # Test case variables...
-    @files_1 = [
-    "documents/accomplishment.xsd",
-    "images/flower01.jpg",
-    "presentations/resources.ppt",
-    "audio/resources.mp3",
-    "images/resources.JPG"
-    ]
-    @files_2 = [
-    "documents/sample.pdf",
-    "images/flower02.jpg",
-    "documents/resources.doc"
-    ]
-    
-    @folder_name = "Folder 1"
+class Goof # Superclass
+  #Only this class manipulates the class variable @@classes
+  def instantiate_class(key)
+    eval(@@classes[key]).new  # This is now problematic because the @@classes variable is not defined outside a method.
     
   end
-  
-  def teardown
-    # Close the browser window
-    @browser.close
+
+  def do_this_thing  # This method needs to be used by all sub classes.
+    puts "Doing it."
+    instantiate_class(:this)
   end
   
-  def test_adding_resources
-    
-    # Log in to Sakai
-    workspace = @sakai.login(@username, @password)
-    
-    # Go to the test site
-    home = workspace.open_my_site_by_id(@site_id)
-    
-    # Go to Resources page
-    resources = home.resources
-    
-    puts resources.folder_names
-    puts resources.file_names
-    
+  def do_other_thing
+      puts "Get funky!"
+      instantiate_class(:other)
+  end
+
+  def set_hash(hash_object)  # This was Abe's idea for the method.
+    @@classes = hash_object
+  end
+
+end
+
+class Mofo < Goof # Sub Class 1
+  
+  def initialize
+    #@@classes = {:this=>"Mofo"}
+    set_hash ({:this=>"Mofo", :other=>"MasterFun"})
   end
   
 end
+
+class Seefeel < Goof # Sub Class 2
+  
+  def initialize
+    set_hash( {:this=>"Seefeel", :other=>"Funkadelic"} )
+  end
+  
+end
+
+class MasterFun < Goof
+
+end
+
+class Funkadelic < Goof
+
+end
+
+fun = Seefeel.new
+
+goo = fun.do_other_thing
+
+puts goo.class
