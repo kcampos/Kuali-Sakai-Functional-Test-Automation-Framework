1610b6562f93b4d6064b0519cde77177ce99a4ed
Abraham Heward <aheward@rsmart.com>
2011-12-08 12:12:24 -0700

    Created some new test cases.
    Edit Profile2 is nearly complete. Add Unofficial Course Guests is
    complete.

--- lib/sakai-CLE/site_page_elements.rb
+++ lib/sakai-CLE/site_page_elements.rb
@@ -5466,6 +5466,82 @@ class PortfolioAttachFiles < AttachPageTools
   
 end
 
+
+#================
+# Roster Pages for a Site
+#================
+
+# 
+class Roster
+  
+  include PageObject
+  include ToolsMenu
+
+  def find
+    frm.button(:value=>"Find").click
+    Roster.new @browser
+  end
+  
+  def names
+    list = []
+    frm.table(:id=>"roster_form:rosterTable").rows.each do |row|
+      list << row[0].text
+    end
+    list.delete_at(0)
+    return list
+  end
+  
+  # Clicks on the link on the page that matches the specified name.
+  # Then instantiates the RosterProfileView class.
+  # Note that it expects an exact match for the name string, otherwise
+  # the script will error out.
+  def view(name)
+    frm.link(:text=>name).click
+    RosterProfileView.new @browser
+  end 
+
+  in_frame(:class=>"portletMainIframe") do |frame|
+    text_field(:name_or_id, :id=>"roster_form:search", :frame=>frame)
+    
+  end
+end
+
+#
+class RosterProfileView
+  
+  include PageObject
+  include ToolsMenu
+    
+  def back
+    frm.button(:value=>"Back").click
+    Roster.new(@browser)
+  end
+
+  # Returns a hash containing the contents of the Public Information
+  # table on the page, with the keys being the row headers and the values
+  # being the row's data contents.
+  def public_information
+    hash = {}
+    frm.table(:class=>"chefEditItem", :index=>0).rows.each do |row|
+      hash.store(row[0].text, row[1].text)
+    end
+    return hash
+  end
+  
+  # Returns a hash containing the contents of the Personal Information
+  # table on the page, with the keys being the row headers and the values
+  # being the row's data contents.
+  def personal_information
+    hash = {}
+    frm.table(:class=>"chefEditItem", :index=>1).rows.each do |row|
+      hash.store(row[0].text, row[1].text)
+    end
+    return hash
+  end
+
+end
+
+
 #================
 # Sections Pages for a Site
 #================
